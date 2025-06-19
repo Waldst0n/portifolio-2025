4acb9546f975efd8d3fa69e489f97eff33c5dc1d
Waldst0n <waldston.ws@gmail.com>
2025-06-19 11:09:28 -0300

    feat: Alterações na Page Home Add links no card

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,30 +1,33 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full flex  h-20 flex items-center justify-center p-4 ">
+    <div className="w-full h-20 flex items-center justify-center p-4 md:mb-24 ">
       <nav>
         <ul className="flex gap-4 sm:gap-16  ">
           <li className=" transform transition-transform duration-200 hover:scale-125">
-            <a
+            <Link
               href="/"
               className="text-lg sm:text-lg font-semibold hover:text-blue-300"
             >
               Início
-            </a>
+            </Link>
           </li>
           <li className=" transform transition-transform duration-200 hover:scale-125">
-            <a
-              href="/about"
+            <Link
+              href="/projects"
               className="text-lg sm:text-lg font-semibold hover:text-blue-300 "
             >
               Projetos
-            </a>
+            </Link>
           </li>
           <li className=" transform transition-transform duration-200 hover:scale-125">
             <a
-              href="/contact"
+              href="https://drive.google.com/file/d/12JaTHGQEhzzewOr56MBFiTZuBjbMxB63/view?usp=sharing"
               className="text-lg sm:text-lg font-semibold hover:text-blue-300  "
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Currículo
             </a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,92 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Facebook, Github, Instagram, Link, Linkedin } from "lucide-react";
 import Image from "next/image";
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
 
 export default function Home() {
-  return <div>teste</div>;
+  return (
+    <div className="w-full flex items-center justify-center p-8">
+      <main className="flex flex-col md:flex-row items-center justify-center w-full h-full gap-12 ">
+        <Card className="min-w-76 flex flex-col justify-around max-h-[800px] bg-[url('/background-card.png')] bg-no-repeat bg-cover bg-bottom border-0 shadow-xl color-background">
+          <CardHeader className="flex flex-col items-center justify-center">
+            <Image
+              src="https://avatars.githubusercontent.com/u/123845800?v=4"
+              alt={"Imagemd eperfil"}
+              width={150}
+              height={150}
+              className="rounded-full"
+            />
+            <CardTitle className="flex flex-col items-center justify-center">
+              <h1 className="text-background text-center text-3xl font-bold">
+                Waldston Santana
+              </h1>
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center justify-center">
+            <CardDescription className="text-background text-2xl text-center">
+              Desenvolvedor Fullstack apaixonado por tecnologia e inovação.
+            </CardDescription>
+          </CardContent>
+          <CardFooter className="flex flex-col items-center justify-center">
+            <div className="flex gap-4">
+              <a
+                href="https://wa.me/5579988232693?text=Olá%2C+vim+pelo+seu+portfólio%21"
+                className="text-background text-blue-300 hover:text-blue-500   transition-colors duration-200"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp size={30} />
+              </a>
+              <a
+                href="https://instagram.com/waldstonsantana"
+                className="text-background text-blue-300 hover:text-blue-500   transition-colors duration-200"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram size={30} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/waldston-santana/"
+                className="text-background text-blue-300 hover:text-blue-500   transition-colors duration-200"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin size={30} />
+              </a>
+              <a
+                href="https://github.com/Waldst0n"
+                className="text-background text-blue-300 hover:text-blue-500   transition-colors duration-200"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub size={30} />
+              </a>
+            </div>
+          </CardFooter>
+        </Card>
+        <div className="flex flex-col items-start justify-center">
+          <h1 className="text-2xl text-center md:text-start md:text-6xl lg:text-7xl font-extrabold leading-tight">
+            DESENVOLVEDOR <span className="text-gray-500">FULLSTACK</span>
+          </h1>
+          <p className="text-lg md:text-xl lg:text-2xl mt-4 text-justify">
+            Sou um desenvolvedor FullStack apaixonado por criar soluções
+            escaláveis e de alto desempenho. Tenho sólida experiência em React e
+            Node.js, além de conhecimentos em bancos de dados SQL e NoSQL, e
+            arquitetura de microsserviços. Trabalho com metodologias ágeis e
+            possuo forte capacidade de colaborar de forma eficiente em equipes
+            dinâmicas.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
