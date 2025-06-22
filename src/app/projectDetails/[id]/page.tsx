78d15b831b0286c55b49492679df3ef00d5530a6
Waldst0n <waldston.ws@gmail.com>
2025-06-22 11:50:57 -0300

    fix: Correcao erro de build

--- src/app/projectDetails/[id]/page.tsx
+++ src/app/projectDetails/[id]/page.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import { getProjetoById } from "@/app/utils/projectsData";
 
-export interface PageDetailsProps {
-  params: {
-    id: string;
-  };
-}
-const Page = ({ params }: PageDetailsProps) => {
+// você não precisa da interface PageDetailsProps aqui
+
+const Page = ({ params }: { params: { id: string } }) => {
   const projeto = getProjetoById(params.id);
 
   if (!projeto) return <div>Projeto não encontrado!</div>;
 
   return (
     <div className="w-full flex items-center justify-center p-8 ">
-      <main
-        className={`flex flex-col  items-center justify-center  w-full h-full gap-12 transition-opacity duration-1000 
-        }`}
-      >
+      <main className="flex flex-col items-center justify-center w-full h-full gap-12 transition-opacity duration-1000">
         <h1 className="text-4xl md:text-[70px] text-center font-extrabold leading-none">
           {projeto.title.toUpperCase()}
         </h1>
